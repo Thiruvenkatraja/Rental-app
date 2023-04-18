e8de62f55ca0ee77ec0294a973d68ac71e85f7cb
vigneshmagudeeswaran <m.vigneshviki1996@gmail.com>
2023-04-19 03:28:29 +0530

    AddClient

--- ce-frontend/src/App.tsx
+++ ce-frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import ResponsiveAppBar from "./components/Utils/HeaderBar";
 import { Admin } from "./components/Admin/Admin";
+import AddClient from "./components/Forms/AddClient";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <ResponsiveAppBar />
       <Routes>
         <Route path="/" element={<Admin />} />
+        <Route path="/addclient" element={<AddClient />}/>
       </Routes>
     </BrowserRouter>
   );
