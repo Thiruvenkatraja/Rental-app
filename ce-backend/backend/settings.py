72dd0b3956a979b336eb0cc679c7284b8d4cc23b
Manoj <666rmanoj@gmail.com>
2023-04-20 02:58:36 +0530

    property_info

--- ce-backend/backend/settings.py
+++ ce-backend/backend/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'client_info',
+    'property_info',
+    'multiselectfield',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +79,11 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'IGS_CE',
+        'USER': 'postgres',
+        'PASSWORD': '12345',
+        'HOST': 'localhost'
     }
 }
 
---
+++ ce-backend/property_info/models.py
@@ -0,0 +1,55 @@
+from django.db import models
+from multiselectfield import MultiSelectField
+# Create your models here.
+
+
+class Property(models.Model):
+    Property_ID = models.AutoField(primary_key=True)
+    PropertyTitle = models.CharField(max_length=100)
+    Property_Choices = (
+        ('1', '1'),
+        ('2', '2'),
+        ('3', '3'),
+    )
+    PropertyType = models.CharField(max_length=100, choices=Property_Choices)
+    Listing_Choices = (
+        ('1', '1'),
+        ('2', '2'),
+        ('3', '3'),
+    )
+    Property_ListingType = models.CharField(
+        max_length=100, choices=Listing_Choices)
+    Property_Location = models.CharField(max_length=100)
+    Property_Address = models.CharField(max_length=200)
+    Property_OverallSqft = models.PositiveIntegerField()
+    Property_Blocks = models.PositiveIntegerField()
+    Property_Floors = models.PositiveIntegerField()
+    Property_Flats = models.PositiveIntegerField()
+    Property_1BHK = models.PositiveIntegerField()
+    Property_2BHK = models.PositiveIntegerField()
+    Property_3BHK = models.PositiveIntegerField()
+    Amenities_List = [
+        ('garden', 'Garden'),
+        ('security_cameras', 'Security Cameras'),
+        ('laundry', 'Laundry'),
+        ('internet', 'Internet'),
+        ('pool', 'Pool'),
+        ('video_surveillance', 'Video Surveillance'),
+        ('jacuzzi', 'Jacuzzi'),
+        ('elevator', 'Elevator'),
+        ('vigilance', 'Vigilance'),
+        ('dish', 'Dish'),
+        ('solar_panel', 'Solar Panel'),
+        ('garage', 'Garage')
+    ]
+    Property_Amenities = MultiSelectField(
+        max_length=120, choices=Amenities_List)
+    Property_ImgURL = models.URLField(max_length=5000)
+
+    class Meta:
+        db_table = 'Property'
+        verbose_name_plural = 'Property'
+
+    def __str__(self):
+        return f"{self.PropertyTitle}"
+    
