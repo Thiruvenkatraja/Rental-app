d7b06a5f1d7eaa677bceddc54b3b4de483b512a0
Manoj <666rmanoj@gmail.com>
2023-04-27 19:43:51 +0530

    added datagrid viewset

--- ce-backend/client_info/views.py
+++ ce-backend/client_info/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from rest_framework.response import Response
 from .models import Clients
-from .serializers import ClientsSerializer, ClientsSerializerID
+from .serializers import ClientsSerializer, ClientsSerializerID,DataGridSerializer
 from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.exceptions import ValidationError
@@ -30,6 +30,38 @@ class ClientsView (APIView):
         data = request.data
         ClientsData = Clients.objects.get(Client_ID=Client_ID)
         serializer = ClientsSerializerID(instance=ClientsData, data=data)
+        if serializer.is_valid():
+            serializer.save()
+        return Response({"Response": "Clients Data is Successfully Updated", "data": serializer.data})
+
+    def delete(self, request, Client_ID, *args, **kwargs):
+        ClientsData = Clients.objects.filter(Client_ID=Client_ID)
+        ClientsData.delete()
+        return Response({"Response": "Clients Data is Successfully Deleted"})
+
+
+class DataGridView(APIView):
+    serializer_class = DataGridSerializer
+    def get(self,request,Client_ID=None):
+        if Client_ID:
+            data = Clients.objects.filter(Client_ID=Client_ID).first()
+            serializer = DataGridSerializer(data)
+        else:
+            data = Clients.objects.all()
+            serializer=DataGridSerializer(data,many=True)
+        return Response(serializer.data)
+        
+    def post(self, request, *args, **kwargs):
+        data = request.data
+        serializer = DataGridSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+        return Response({"Response": "Clients Data is Successfully saved", "data": serializer.data})
+
+    def put(self, request, Client_ID, *args, **kwargs):
+        data = request.data
+        ClientsData = Clients.objects.filter(Client_ID=Client_ID).first()
+        serializer = DataGridSerializer(instance=ClientsData, data=data)
         if serializer.is_valid():
             serializer.save()
         return Response({"Response": "Clients Data is Successfully Updated", "data": serializer.data})
